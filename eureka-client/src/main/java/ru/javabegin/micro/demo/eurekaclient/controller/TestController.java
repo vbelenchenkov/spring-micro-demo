0fd177b688c40d3ccc6dcc6cd95c65ea076ceec1
viacheslav belenchenkov <belenchenkov@gmail.com>
2026-06-13 22:04:27 +0300

    lesson 16

--- eureka-client/src/main/java/ru/javabegin/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/javabegin/micro/demo/eurekaclient/controller/TestController.java
@@ -2,9 +2,11 @@ package ru.javabegin.micro.demo.eurekaclient.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
     @GetMapping("/test")
